6f38d0220397dc1f0eb0ed87cb84614fd5a4d6ea
Baroxyton <69308275+baroxyton@users.noreply.github.com>
2023-02-07 13:50:28 +0100

    Clean up config.js

--- config.js
+++ config.js
@@ -4,40 +4,24 @@ import envPaths from 'env-paths';
 const configPath = envPaths('zlibCLI').config;
 
 const configSchema = {
-	isLoggedIn: {
-		type: 'boolean',
-		"default":false
-	},
-	installedBooks: {
-		type: 'array',
-		"default": []	
-	},
-	userid:{
-		type:'string',
-		"default":"undefined"
-	},
-	userkey:{
-		type:'string',
-		"default":"undefined"
-	},
-	domain:{
-		type:'string',
-		'default':"https://1lib.ch"
-	},
-	lang:{
-		type:"string",
-		"default":"EN"
-	},
-	downloadPath:{
-		type:"string",
-		default:configPath + "/downloads"
-	}
-}
+	isLoggedIn: {type: 'boolean', default: false},
+	installedBooks: {type: 'array', default: []},
+	userid: {type: 'string', default: undefined},
+	userkey: {type: 'string', default: undefined},
+	domain: {type: 'string', default: 'https://1lib.ch'},
+	lang: {type: 'string', default: 'EN'},
+	downloadPath: {type: 'string', default: configPath + '/downloads'},
+};
 
 const config = new Conf ({projectName: 'zlibCLI', schema:configSchema});
 function isLoggedIn(){
 	return config.get("isLoggedIn"); 
 }
+
+/**
+ * @param userid {string}
+ * @param userkey {string}
+ */
 function login(userid, userkey){
 	config.set("isLoggedIn", true);
 	config.set("userid", userid);
@@ -49,14 +33,25 @@ function logout(){
 	config.set("userid", "undefined");
 	config.set("userkey", "undefined");
 }
+/**
+ * @param bookID {number}
+ */
 function addDownloadedBook(bookID){
 	let downloads = config.get("installedBooks");
 	downloads.push(bookID);
 	config.set("installedBooks", downloads);
 }
+
+/**
+ * @param url {string}
+ */
 function setMirror(url){
 	config.set("domain", url);
 }
+
+/**
+ * @param lang {string}
+ */
 function setLang(lang){
 	config.set("lang", lang);
 }
@@ -66,6 +61,10 @@ function getLogin(){
 function getMirror(){
 	return config.get("domain");
 }
+
+/**
+ * @param bookid {number}
+ */
 function isBookInstalled(bookid){
 	return config.get("installedBooks").includes(bookid);
 }
