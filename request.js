87489ce245f1ccd93c4e89016e8494ca12bf9414
Baroxyton <69308275+baroxyton@users.noreply.github.com>
2023-02-07 21:55:40 +0100

    Add request.js

--- request.js
+++ request.js
@@ -1,1 +1,16 @@
 import axios from 'axios';
+/**
+* @param endpoint {string}
+* @param {object}
+*/
+async function GETRequest(endpoint, query){
+
+}
+/**
+* @param endpoint {string}
+* @param params {object}
+*/
+async function POSTRequest(endpoint, params){
+
+}
+export default {GETRequest, POSTRequest};
