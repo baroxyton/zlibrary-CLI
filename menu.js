31298721ef9c5d41c44c80fe5cba90608bab23c2
Baroxyton <69308275+baroxyton@users.noreply.github.com>
2023-02-08 16:22:45 +0100

    Add support for logouts in start menu

--- menu.js
+++ menu.js
@@ -1,11 +1,52 @@
 import Enquirer from 'enquirer';
-function startMenu(){
+import configs from './config.js';
+import api from './api.js'
+async function startMenu(){
+	console.clear();
+	let accountPrompt = configs.isLoggedIn()?"Sign out":"Log in/Sign up";
 	const prompt = new Enquirer.Select({
-		name: 'color',
+		name: 'startmenu',
 		message: 'What would you like to do?',
-		choices: ["Log In/Sign up", "Seach z-library", "Browse downloaded books", "settings"]
+		choices: [accountPrompt, "Seach z-library", "Browse downloaded books", "settings"]
 	});
-	let result = prompt.run();
-	console.log(result)
+	let result = await prompt.run();
+	console.log("RESULT:" + result)
+	switch(result){
+		case "Sign out": 
+			api.logout();
+			startMenu();
+			break;
+		case "Log in/Sign up":
+			loginMenu();
+			break;
+		case "Search z-library":
+			searchMenu();
+			break;
+		case "Browse downloaded books":
+			openDownloads();
+			break;
+		case "settings":
+			settingsMenu();
+			break;
+	}
 }
+function settingsMenu(){
+
+}
+function searchMenu(){}
+async function loginMenu(){
+	console.clear();
+	const prompt = new Enquirer.Select({
+		name: "loginMenu",
+		message: "What would you like to do?",
+		choices: ["Log in", "Sign up", "Log in with user key", "Back"]
+	});
+	let result = await prompt.run();
+	switch(result){
+		case "Back": 
+			startMenu();
+			break;
+	}
+}
+function openDownloads(){}
 startMenu();
