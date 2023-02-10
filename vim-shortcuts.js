cc12d1eadad24ea4be2674044f5188eecbf44622
Baroxyton <69308275+baroxyton@users.noreply.github.com>
2023-02-10 23:19:03 +0100

    Only use vim shortcuts when specified

--- vim-shortcuts.js
+++ vim-shortcuts.js
@@ -1,4 +1,4 @@
 import actions from 'enquirer/lib/combos.js'
 const vimShortcuts = { h: 'left', j: 'down', k: 'up', l: 'right' };
-actions.keys = { ...actions.keys, ...vimShortcuts };
-export default actions
+let vimActions =  { ...actions.keys, ...vimShortcuts };
+export default {...actions, keys:vimActions}
